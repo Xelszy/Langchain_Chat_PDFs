7d3ba23812414dd7920be392b8ae0c78d3d5768f
Deo.SiriliusVansaHavian <48593365+Xelszy@users.noreply.github.com>
2023-06-23 19:00:03 +0700

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -1,46 +1,37 @@
 css = '''
 <style>
-    .chat-message {
-        padding: 10px;
-        margin-bottom: 10px;
-        display: flex;
-        align-items: flex-start;
-    }
-
-    .chat-message.user {
-        justify-content: flex-end;
-    }
-
-    .chat-message.bot {
-        justify-content: flex-start;
-    }
-
-    .chat-message .avatar {
-        display: none;
-    }
-
-    .chat-message .message {
-        max-width: 70%;
-        padding: 10px;
-        border-radius: 10px;
-        color: #fff;
-        word-wrap: break-word;
-        backdrop-filter: blur(10px); /* Efek glassmorphism */
-        background-color: rgba(255, 255, 255, 0.2); /* Warna latar belakang dengan transparansi */
-    }
-
-    .chat-message.user .message {
-        color: #000;
-        align-self: flex-end;
-        background-color: rgba(220, 248, 198, 0.5); /* Warna latar belakang user */
-    }
-
-    .chat-message.bot .message {
-        color: #000;
-        align-self: flex-start;
-        background-color: rgba(255, 255, 255, 0.5); /* Warna latar belakang bot */
-    }
-</style>
+.chat-message {
+    padding: 10px;
+    margin-bottom: 10px;
+    display: flex;
+    align-items: flex-start;
+}
+.chat-message.user {
+    justify-content: flex-end;
+}
+.chat-message.bot {
+    justify-content: flex-start;
+}
+.chat-message .avatar {
+    display: none;
+}
+.chat-message .message {
+    max-width: 70%;
+    padding: 10px;
+    border-radius: 10px;
+    color: #fff;
+    word-wrap: break-word;
+}
+.chat-message.user .message {
+    color: #000;
+    background-color: #dcf8c6;
+    align-self: flex-end;
+}
+.chat-message.bot .message {
+    background-color: #fff;
+    color: #000;
+    align-self: flex-start;
+}
 '''
 
 bot_template = '''
